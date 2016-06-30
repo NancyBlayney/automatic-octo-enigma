7dae8fb0e4eea71db54d416af934674455188356
Nancy Blayney <nblayney@g.cofc.edu>
2016-06-30 10:43:34 -0400

    progress

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <?php
 
 	class User{
-		public function __construct(){
+		public $id;
+		public $name;
+		public $email;
+		public $pword;
+
+		public function __construct($name, $pword){
+			$this->name = $name;
+			$this->pword = $pword;
 			echo 'Constructor Called';
 		}
 
-		public function register(){
-			echo "registered";
+		// public function register(){
+		// 	echo "registered";
+		// }
+
+		public function login(){
+			$this->auth_user();
+			
 		}
 
-		public function login($name, $pword){
-			$this->auth_user($name, $pword);
-			echo $name . " is now logged in";
+		public function auth_user(){
+			echo $this->name . " is now the boss";
 		}
 
-		public function auth_user($name, $pword){
-			echo $name . " is now the boss";
-		}
-
-		public function __destruct(){
-			echo 'destructor called';
-		}
+		// public function __destruct(){
+		// 	echo 'destructor called';
+		// }
 	}
 
-	$me = new User;
-	$me->register();
-	$me->login('nancy', '123');
+	$me = new User('nancy', '123');
+	// $me->register();
+	$me->login();
 ?>
